2c62bdedb823bec10079efaf47dee1191edfe45f
davecalleja-merkle <90806540+davecalleja-merkle@users.noreply.github.com>
2025-10-03 18:17:22 +0800

    Add .storybook/main.ts via Plop.js design system scaffolding

---
+++ .storybook/main.ts
@@ -0,0 +1,28 @@
+import type { StorybookConfig } from '@storybook/nextjs';
+
+const config: StorybookConfig = {
+  // Stories co-located with components (Storybook v9 best practice)
+  stories: [
+    '../components/**/*.stories.@(js|jsx|ts|tsx|mdx)',
+    '../app/**/*.stories.@(js|jsx|ts|tsx|mdx)',
+  ],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@chromatic-com/storybook',
+    '@storybook/addon-a11y'
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {
+      nextConfigPath: '../next.config.js',
+    },
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+  staticDirs: ['../public'],
+};
+
+export default config;
